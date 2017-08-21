00e95f9d1ad2e67067c8ca9e8dd1bfa4d2b96abd
jbe2277 <jbe2277@users.noreply.github.com>
2017-08-21 23:29:55 +0200

    Upgrade System.Waf.*.Core to .NET Standard 2.0

--- src/System.Waf/System.Waf/System.Waf.Core/Properties/AssemblyInfo.cs
+++ src/System.Waf/System.Waf/System.Waf.Core/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Reflection;
+﻿//using System;
+//using System.Reflection;
 
-[assembly: AssemblyTitle("System.Waf.Core")]
-[assembly: AssemblyDescription("System.Waf is a lightweight framework that helps you to create well-structured XAML applications. "
-    + "This is the portable core part that can be used in different application types.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("System.Waf.Core")]
+//[assembly: AssemblyTitle("System.Waf.Core")]
+//[assembly: AssemblyDescription("System.Waf is a lightweight framework that helps you to create well-structured XAML applications. "
+//    + "This is the portable core part that can be used in different application types.")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyProduct("System.Waf.Core")]
 
-[assembly: CLSCompliant(true)]
+//[assembly: CLSCompliant(true)]
